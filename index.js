a78784f7475d97888c18644c6ae76e475d8eb110
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-07-22 20:48:59 +0600

    All COllege data API implementation DOne

--- index.js
+++ index.js
@@ -26,6 +26,46 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const usersCollection = client.db('collegeBooking').collection('user');
+        const collegesCollection = client.db('collegeBooking').collection('colleges');
+
+        // User Added API in the MONGODB Database
+        app.post('/user', async (req, res) => {
+            try {
+                const body = req.body;
+                const existingUser = await usersCollection.findOne({ email: body.email });
+
+                if (existingUser) {
+                    // If the user already exists, return an error response
+                    return res.status(400).json({ error: "User already exists." });
+                }
+
+                const result = await usersCollection.insertOne(body);
+                res.send(result)
+            } catch (error) {
+                console.error("Error during user signup:", error);
+                res.status(500).json({ error: "An internal server error occurred." });
+            }
+        });
+
+        // Get All College Data
+        app.get('/colleges', async (req, res) => {
+            try {
+                const allColleges = await collegesCollection.find({}).toArray();
+                res.json(allColleges);
+            } catch (error) {
+                console.error("Error retrieving colleges data:", error);
+                res.status(500).json({ error: "An internal server error occurred." });
+            }
+        });
+
+
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
